e1758e2e1a2e39ed411825f6186a6768edb5858b
Benjamin Delahaye <benjamin.delahaye@polyconseil.fr>
2016-05-05 19:04:33 +0200

    update models

--- intranet/cra/admin.py
+++ intranet/cra/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Employe
+from .models import Employe,Consultant
 
 admin.site.register(Employe)
+admin.site.register(Consultant)
--- intranet/cra/models.py
+++ intranet/cra/models.py
@@ -4,7 +4,27 @@ from django.db import models
 class Employe(models.Model):
     nom = models.CharField(max_length=200)
     prenom = models.CharField(max_length=200)
-    date_embauche = models.DateTimeField('date published')
+    date_embauche = models.DateTimeField("date d'embauche")
 
     def __str__(self):
         return self.nom + "  " + self.prenom
+
+class Mission(models.Model):
+    nomClient = models.CharField(max_length=200)
+    dateDebutMission = models.DateTimeField("date de debut de mission") 
+    dateFinMission = models.DateTimeField("date de fin de mission")
+
+class Consultant(Employe):
+    missionsEffectues = []
+
+class Mission(models.Model):
+    date_debut = models.DateField("Date de début de mission")
+    date_fin = models.DateField("Date de fin de mission")
+    client = Client
+
+class Client(models.Model):n
+    nom = models.CharField(max_length=200)
+    prenom = models.CharField(max_length=200)
+    societe = models.CharField(max_length=200)
+
+        
