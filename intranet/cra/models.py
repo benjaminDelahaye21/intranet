985aa8941b2d297738739662c77600c8249939a2
Benjamin Delahaye <benjamin.delahaye@polyconseil.fr>
2016-05-05 13:15:28 +0200

    set models

--- intranet/cra/models.py
+++ intranet/cra/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Employe(models.Model):
+    nom = models.CharField(max_length=200)
+    prenom = models.CharField(max_length=200)
+    date_embauche = models.DateTimeField('date published')
